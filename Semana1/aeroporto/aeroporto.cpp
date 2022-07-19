050ad3aa19d0ef63da9fbab2aef02c17999c065e
Osvaldo Rodrigues De Faria Junior <80057661+OsvaldoUfla@users.noreply.github.com>
2022-07-19 19:55:46 -0300

    ,

--- Semana1/aeroporto/aeroporto.cpp
+++ Semana1/aeroporto/aeroporto.cpp
@@ -5,60 +5,10 @@ using namespace std;
 
 int main()
 {
-	int m;
-	int n;  
-	int maior = -1;
-	int quantTest=0;
-	cin >> m >>n;
-	vector<int> vectorx[2];
-	while(m > 0)
-	{
+int n,m;
+cin >> n >>m;
 
-		int aeroportos[m];
-		for(int j=0 ; j<m ; j++)
-		 {
-			aeroportos[j] = 0;
-		 }
-		maior=-1;
-		for(int j=0 ; j<n ; j++)
-		 {
-			int u,v;
-			cin >> u >>v;
-			aeroportos[u-1]++;
-			aeroportos[v-1]++;
-		 }
 
-		for(int k =0 ; k<m ; k++)
-		{
-			if(aeroportos[k] > maior)
-			{
-				maior = aeroportos[k];
-			}
-		}
-		
-		
-		for(int i=0 ; i<m ; i++)
-		{
-			if(aeroportos[i] == maior)
-			{
-				vectorx[quantTest].push_back(i);
-			}
-		}
 
-		
-		quantTest++;
-		cin >> m >>n;
-
-	}
-
-	for(int i =0; i<quantTest;i++)
-	{
-		cout << "Teste " << i +1 <<endl;
-		for(vector<int>::iterator it = vectorx[i].begin(); it != vectorx[i].end(); ++it)
-		{
-			cout << (*it) + 1 << " ";
-		}
-		cout << endl;
-	}
-	return 0;
+return 0;
 }
